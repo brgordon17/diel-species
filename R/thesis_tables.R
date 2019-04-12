ee8f2961b453090cd6b9d7863134a6dced3fded6
Benjamin R. Gordon <brgordon17@gmail.com>
2019-04-12 11:57:01 +1000

    Initial commit

---
+++ R/thesis_tables.R
@@ -0,0 +1,5 @@
+# Script for producing tables used in Chapter 6 (Heron 24 h)
+# Author: Benjamin R. Gordon
+# Date: 2019-04-12
+
+# diurnal model results --------------------------------------------------------
