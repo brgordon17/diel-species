01a1d3b643519906b660fd86a882447bc82b57e2
Benjamin R. Gordon <brgordon17@gmail.com>
2019-04-15 21:57:11 +1000

    Initial commit

---
+++ R/snapdata_raw.R
@@ -0,0 +1,9 @@
+# Feature detection and annotation of the snap-freeze and methanol-quenched 
+# LCMS datat using xcms and CAMERA. For the Heron-diurnal chapter 6.
+# Author: Benjamin Gordon 
+# Date: 8-JAN-2019
+# Adapted from ~/gordonC6/R/mzdata_raw.R
+
+library(xcms)
+library(magrittr)
+library(CAMERA)
