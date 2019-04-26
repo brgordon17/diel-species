89f8371c33930193915f979bc8f009db1756157d
Benjamin R. Gordon <brgordon17@gmail.com>
2019-04-26 11:55:21 +1000

    Initial commit

---
+++ R/crossref.R
@@ -0,0 +1,8 @@
+# Script to cross reference important diurnal features with the coral
+# research literature.
+# Author: Benjamin R. Gordon
+# Date: 2019-04-26
+
+library(tidyverse)
+library(caret)
+
