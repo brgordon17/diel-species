41224a56b8b9052caf92099ea5cb2e4dc4b026ee
Benjamin R. Gordon <brgordon17@gmail.com>
2019-05-03 13:47:02 +1000

    added code to plot and examine mz135 in m. digitata.

--- R/thesis_figures.R
+++ R/thesis_figures.R
@@ -1208,3 +1208,34 @@ ggplot(sum_mzdata, aes(x = time_fac,
   )
 
 ggsave("./figs/diurnal_intensities.pdf")
+
+# plot of mdigitata m/z 135. Not printed
+ggplot(filter(sum_mzdata, taxon == "M_digi" & variable == "135.04778"),
+       aes(x = time_fac,
+           y = mean,
+           fill = taxon,
+           colour = taxon,
+           shape = taxon,
+           group = taxon)) +
+  geom_errorbar(aes(ymin = mean - se, ymax = mean + se), 
+                width = .1,
+                show.legend = FALSE) +
+  geom_path(size = 0.8, show.legend = FALSE) +
+  geom_point(size = 3) +
+  geom_hline(yintercept = 0.1, linetype = 3) +
+  scale_x_discrete(name = "Time (hh:mm)") +
+  scale_y_continuous(name = Mean~Intensity%*%10^4) +
+  scale_color_manual(values = grDevices::adjustcolor(gordon01::qual_colours[c(1:3, 6, 7)],
+                                                     alpha.f = 0.9)) +
+  scale_fill_manual(values = grDevices::adjustcolor(gordon01::qual_colours[c(1:3, 6, 7)],
+                                                    alpha.f = 0.5)) +
+  scale_shape_manual(values = c(21:25)) +
+  theme(axis.text = element_text(size = 12, colour = "grey65"),
+        axis.title = element_text(size = 16),
+        axis.ticks = element_blank(),
+        legend.key = element_blank(),
+        legend.text = element_text(size = 14),
+        legend.title = element_blank(),
+        strip.text = element_text(size = 14),
+        strip.background = element_blank()
+  )
