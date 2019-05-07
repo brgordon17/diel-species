734c521ee5f19929ee7de688258f42aa6b48ac54
Benjamin R. Gordon <brgordon17@gmail.com>
2019-05-07 21:55:52 +1000

    modified code so the kruskal-wallis test is applied to each species individually.

--- R/important_features_stats.R
+++ R/important_features_stats.R
@@ -8,9 +8,11 @@ library(tidyverse)
 load("./data/mzdata.rda")
 matches <- read_rds("./dev/impvar_matches.rds")
 
-features <- 
+# one way anovas (kruskal-wallis test) for each species ------------------------
+# for A. aspera
+features_aspe <- 
   mzdata %>%
-  filter(taxon != "PBQC") %>%
+  filter(taxon != "PBQC" | taxon == "A_aspe") %>%
   droplevels() %>%
   select(1:4,
          mz_784.53491,
@@ -21,8 +23,30 @@ features <-
          mz_135.04778)
 
 # test for normality (normal if p > 0.05)
-apply(features[5:ncol(features)], 2, shapiro.test)
+apply(features_aspe[5:ncol(features_aspe)], 2, shapiro.test)
 
 # all of the variables are non-normal so perform a Kruskal-Wallis test
 # on all variables within each species
-apply(features[5:ncol(features)], 2, kruskal.test, g = features$time_fac)
+apply(features_aspe[5:ncol(features_aspe)], 2, kruskal.test, 
+      g = features_aspe$time_fac)
+
+# For 
+features_aspe <- 
+  mzdata %>%
+  filter(taxon != "PBQC" | taxon == "A_aspe") %>%
+  droplevels() %>%
+  select(1:4,
+         mz_784.53491,
+         mz_277.18008,
+         mz_1019.77533,
+         mz_347.25863,
+         mz_815.55253,
+         mz_135.04778)
+
+# test for normality (normal if p > 0.05)
+apply(features_aspe[5:ncol(features_aspe)], 2, shapiro.test)
+
+# all of the variables are non-normal so perform a Kruskal-Wallis test
+# on all variables within each species
+apply(features_aspe[5:ncol(features_aspe)], 2, kruskal.test, 
+      g = features_aspe$time_fac)
