28276421ddcec0d3c1e25b94a290a321560201ca
Benjamin R. Gordon <brgordon17@gmail.com>
2019-05-08 00:02:33 +1000

    minor formatting

--- R/important_features_stats.R
+++ R/important_features_stats.R
@@ -12,7 +12,7 @@ matches <- read_rds("./dev/impvar_matches.rds")
 # for A. aspera
 features_aspe <- 
   mzdata %>%
-  filter(taxon != "PBQC" | taxon == "A_aspe") %>%
+  filter(taxon != "PBQC" & taxon == "A_aspe") %>%
   droplevels() %>%
   select(1:4,
          mz_784.53491,
@@ -25,15 +25,14 @@ features_aspe <-
 # test for normality (normal if p > 0.05)
 apply(features_aspe[5:ncol(features_aspe)], 2, shapiro.test)
 
-# all of the variables are non-normal so perform a Kruskal-Wallis test
-# on all variables within each species
+# some of the variables are non-normal so perform a Kruskal-Wallis test
 apply(features_aspe[5:ncol(features_aspe)], 2, kruskal.test, 
       g = features_aspe$time_fac)
 
-# For 
-features_aspe <- 
+# For M. digitata
+features_digi <- 
   mzdata %>%
-  filter(taxon != "PBQC" | taxon == "A_aspe") %>%
+  filter(taxon != "PBQC" & taxon == "M_digi") %>%
   droplevels() %>%
   select(1:4,
          mz_784.53491,
@@ -44,9 +43,9 @@ features_aspe <-
          mz_135.04778)
 
 # test for normality (normal if p > 0.05)
-apply(features_aspe[5:ncol(features_aspe)], 2, shapiro.test)
+apply(features_digi[5:ncol(features_digi)], 2, shapiro.test)
 
 # all of the variables are non-normal so perform a Kruskal-Wallis test
 # on all variables within each species
-apply(features_aspe[5:ncol(features_aspe)], 2, kruskal.test, 
-      g = features_aspe$time_fac)
+apply(features_digi[5:ncol(features_digi)], 2, kruskal.test, 
+      g = features_digi$time_fac)
