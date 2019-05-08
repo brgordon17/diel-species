8138214800e08aae49dc7089d93fe2811caaa52f
Benjamin R. Gordon <brgordon17@gmail.com>
2019-05-08 22:58:02 +1000

    added code to perform one-way ANOVA for each feature within each species.

--- R/important_features_stats.R
+++ R/important_features_stats.R
@@ -8,8 +8,7 @@ library(tidyverse)
 load("./data/mzdata.rda")
 matches <- read_rds("./dev/impvar_matches.rds")
 
-# one way anovas (kruskal-wallis test) for each species ------------------------
-# for A. aspera
+# one way anovas for A. aspera -------------------------------------------------
 features_aspe <- 
   mzdata %>%
   filter(taxon != "PBQC" & taxon == "A_aspe") %>%
@@ -22,14 +21,30 @@ features_aspe <-
          mz_815.55253,
          mz_135.04778)
 
-# test for normality (normal if p > 0.05)
-apply(features_aspe[5:ncol(features_aspe)], 2, shapiro.test)
+formula <- 
+  as.formula(paste0("cbind(", paste(names(features_aspe)[5:ncol(features_aspe)], 
+                                    collapse = ","), ") ~ time_fac"))
+summary(aov(formula, data = features_aspe))
 
-# some of the variables are non-normal so perform a Kruskal-Wallis test
-apply(features_aspe[5:ncol(features_aspe)], 2, kruskal.test, 
-      g = features_aspe$time_fac)
+# one way anovas for M. aequ ---------------------------------------------------
+features_aequ <- 
+  mzdata %>%
+  filter(taxon != "PBQC" & taxon == "M_aequ") %>%
+  droplevels() %>%
+  select(1:4,
+         mz_784.53491,
+         mz_277.18008,
+         mz_1019.77533,
+         mz_347.25863,
+         mz_815.55253,
+         mz_135.04778)
 
-# For M. digitata
+formula <- 
+  as.formula(paste0("cbind(", paste(names(features_aequ)[5:ncol(features_aequ)], 
+                                    collapse = ","), ") ~ time_fac"))
+summary(aov(formula, data = features_aequ))
+
+# one way anovas for M. digitata -----------------------------------------------
 features_digi <- 
   mzdata %>%
   filter(taxon != "PBQC" & taxon == "M_digi") %>%
@@ -42,10 +57,43 @@ features_digi <-
          mz_815.55253,
          mz_135.04778)
 
-# test for normality (normal if p > 0.05)
-apply(features_digi[5:ncol(features_digi)], 2, shapiro.test)
+formula <- 
+  as.formula(paste0("cbind(", paste(names(features_digi)[5:ncol(features_digi)], 
+                                    collapse = ","), ") ~ time_fac"))
+summary(aov(formula, data = features_digi))
 
-# all of the variables are non-normal so perform a Kruskal-Wallis test
-# on all variables within each species
-apply(features_digi[5:ncol(features_digi)], 2, kruskal.test, 
-      g = features_digi$time_fac)
+# one way anovas for P. cylindrica ---------------------------------------------
+features_cyli <- 
+  mzdata %>%
+  filter(taxon != "PBQC" & taxon == "P_cyli") %>%
+  droplevels() %>%
+  select(1:4,
+         mz_784.53491,
+         mz_277.18008,
+         mz_1019.77533,
+         mz_347.25863,
+         mz_815.55253,
+         mz_135.04778)
+
+formula <- 
+  as.formula(paste0("cbind(", paste(names(features_cyli)[5:ncol(features_cyli)], 
+                                    collapse = ","), ") ~ time_fac"))
+summary(aov(formula, data = features_cyli))
+
+# one way anovas for P. damicornis ---------------------------------------------
+features_dami <- 
+  mzdata %>%
+  filter(taxon != "PBQC" & taxon == "P_dami") %>%
+  droplevels() %>%
+  select(1:4,
+         mz_784.53491,
+         mz_277.18008,
+         mz_1019.77533,
+         mz_347.25863,
+         mz_815.55253,
+         mz_135.04778)
+
+formula <- 
+  as.formula(paste0("cbind(", paste(names(features_dami)[5:ncol(features_dami)], 
+                                    collapse = ","), ") ~ time_fac"))
+summary(aov(formula, data = features_dami))
