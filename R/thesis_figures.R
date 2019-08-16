a88456e298ff9c98733ee32cb38532542e3cf98a
Benjamin R. Gordon <brgordon17@gmail.com>
2019-08-16 11:36:43 +1000

    Deprecated. Use individual thesis_*.R scripts

--- R/thesis_figures.R
+++ R/thesis_figures.R
@@ -1,3 +1,6 @@
+# -------------------- DEPRECATED --------------------------------------------- 
+# Use individual figure_*.R scripts 
+
 # Script to construct figures in chapter 6 (heron 24 h)
 # Author: Benjamin R. Gordon
 # Date: 2019-04-12
