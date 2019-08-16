a351b4a68250254504420ba6d86980237b97a3c8
Benjamin R. Gordon <brgordon17@gmail.com>
2019-08-16 11:42:37 +1000

    Deprecated. Use individual table_*.R scripts

--- R/thesis_tables.R
+++ R/thesis_tables.R
@@ -1,3 +1,6 @@
+# ---------------- DEPRECATED --------------------------------------------------
+# Use individual table_*.R scripts
+
 # Script for producing tables used in Chapter 6 (Heron 24 h)
 # Author: Benjamin R. Gordon
 # Date: 2019-04-12
